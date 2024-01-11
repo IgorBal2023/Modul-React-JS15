da0e9879b0c0dfac6348612c78e9c748888eea01
IgorBal2023 <protuberanec2023@gmail.com>
2024-01-11 11:21:52 +0200

    correct store

--- src/store/changeColorBtn/actions.js
+++ src/store/changeColorBtn/actions.js
@@ -2,7 +2,7 @@ export const SET_ACTIVE_COLOR = "SET_ACTIVE_COLOR";
 
 export const setActiveColor = (color) => {
   return {
-    type: `SET_ACTIVE_COLOR`,
+    type: SET_ACTIVE_COLOR,
     payload: color,
   };
 };
